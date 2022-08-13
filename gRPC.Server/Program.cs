345ecbfec386457a20f3878923aa6017de156218
ahmedlotfiali <ahmedlotfiali@gmail.com>
2022-08-13 14:26:36 +0400

    adding server boilerplate code

--- gRPC.Server/Program.cs
+++ gRPC.Server/Program.cs
@@ -1,9 +1,43 @@
-﻿namespace gRPC.Server
+﻿using Grpc.Core;
+using System;
+using System.IO;
+using gRPCServer = Grpc.Core.Server;
+
+namespace gRPC.Server
 {
     internal class Program
     {
+        const int Port = 50051;
+
         static void Main(string[] args)
         {
+            gRPCServer server = null;
+
+            try
+            {
+                server = new gRPCServer()
+                {
+                    Ports = {
+                        new ServerPort("localhost", Port, ServerCredentials.Insecure)
+                    }
+                };
+
+                server.Start();
+
+                Console.WriteLine("The server is listening on port: {0}", Port);
+                Console.ReadKey();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("The server is failed to start, {0}", ex.Message);
+            }
+            finally
+            {
+                if (server != null)
+                {
+                    server.ShutdownAsync().Wait();
+                }
+            }
         }
     }
 }
