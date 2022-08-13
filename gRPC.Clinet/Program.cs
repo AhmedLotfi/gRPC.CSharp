ae77e4a815cf136a67785ce0df7cf27437e977b6
ahmedlotfiali <ahmedlotfiali@gmail.com>
2022-08-13 14:38:36 +0400

    adding client code to connect with the server

--- gRPC.Clinet/Program.cs
+++ gRPC.Clinet/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace gRPC.Clinet
+﻿using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+using static Dummy.DummyService;
+
+namespace gRPC.Clinet
 {
     internal class Program
     {
+        const string target = "127.0.0.1:50051";
+
         static void Main(string[] args)
         {
+            Channel channel = new Channel(target, ChannelCredentials.Insecure);
+
+            channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                {
+                    Console.WriteLine("The client connected successfully..");
+                }
+            });
+
+            DummyServiceClient client = new DummyServiceClient(channel);
+
+            channel.ShutdownAsync().Wait();
+
+            Console.ReadKey();
         }
     }
 }
