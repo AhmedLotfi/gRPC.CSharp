b6a372b99233fb658e3ca4b4da05940801222406
ahmedlotfiali <ahmedlotfiali@gmail.com>
2022-08-21 00:35:18 +0400

    core: connecting client to running  great server on local docker container with importing server proto buf

--- _Core/GrpcService.Core.Client/Program.cs
+++ _Core/GrpcService.Core.Client/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Triggerd");
+﻿using Grpc.Net.Client;
+using GrpcService.Core;
+using static GrpcService.Core.Greeter;
+
+GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:49163");
+
+GreeterClient client = new GreeterClient(channel);
+
+Console.WriteLine("Client connected...");
+
+Console.WriteLine("Enter yout name:");
+
+HelloReply result = client.SayHello(new HelloRequest
+{
+    Name = Console.ReadLine()
+});
+
+Console.WriteLine(result.Message);
+
+Console.ReadKey();
+
+channel.ShutdownAsync().Wait();
